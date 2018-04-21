6727b912a8d9450436d169351f759cc90517c0b7
Abhishek Chatrath <abhishekchatrath@gmail.com>
2018-04-21 13:40:24 -0700

    Test changes in deploy on heroku

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import logo from './leo.PNG';
 import './App.css';
 
 class App extends Component {
